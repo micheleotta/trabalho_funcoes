db44ede4f24313d9628481526466b806eb39e878
micheleotta <131482012+micheleotta@users.noreply.github.com>
2023-09-12 09:09:14 -0300

    Create funcoes.py

---
+++ funcoes.py
@@ -0,0 +1,10 @@
+Agora que já exercitou os conceitos faça um programa capaz de realizar:
+(g ° f), (g ° g), (f° f) e (f ° g). Para quaisquer f(x) e g(x) que forem dados como
+entrada.
+Exemplo:
+F(x) = x² (representar como x^2)
+G(x) = x-1
+Por fim, alguma operação como (g° f)(4) deve ser possível (utilize os
+exemplos do exercício 6 para testar).
+Formule ao menos 3 exemplos com o programa rodando e ponha no
+relatório
