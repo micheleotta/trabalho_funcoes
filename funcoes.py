c710f0cdd22a68efaa47a0c54c844b98c4ef8949
micheleotta <131482012+micheleotta@users.noreply.github.com>
2023-09-12 10:23:59 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,13 +1,3 @@
-# FUNCAO F(x) = x + 1
-def funcao_f(x):
-  return x + 1
-
-
-# FUNCAO g(X) = X - 5
-def funcao_g(x):
-  return x - 5
-
-
 def gf():
   return funcao_g(funcao_f(valor_de_x))
 
@@ -30,16 +20,67 @@ while operacoes != 2:
   operacoes = int(input("\n\nSelecione a opção: \n1 - Fazer operações com função \n2 - Sair\n"))
 
   if operacoes == 1:
-    #f = int(input("Insira a função f(x) = "))
-    #g = int(input("Insira a função g(x) = "))
-
+    operadorf = int(input("\n\nO que você deseja fazer na função f(x)?\n1 - Somar\n2 - Subtrair \n3 - Multiplicar \n4 - Dividir\n\n"))
+    valorf = int(input("\nInsira o valor para usar na função f(x):\n"))
     
-    valor_de_x = int(input("Insira um valor: "))
+    if operadorf == 1:
+      def funcao_f(x):
+        return x + valorf
+      print(f"\n\nFUNÇÃO F(x) = x + {valorf}")
+    
+    elif operadorf == 2:
+      def funcao_f(x):
+        return x - valorf
+      print(f"\n\nFUNÇÃO F(x) = x - {valorf}")
+
+    elif operadorf == 3:
+      def funcao_f(x):
+        return x * valorf
+      print(f"\n\nFUNÇÃO F(x) = x ⋅ {valorf}")
+
+
+    elif operadorf == 4:
+      def funcao_f(x):
+        return x / valorf
+      print(f"\n\nFUNÇÃO F(x) = x ÷ {valorf}")
+    
+    else:
+      print("\nOpção inválida!")
+    
+
+    operadorg = int(input("\n\nO que você deseja fazer na função g(x)?\n1 - Somar\n2 - Subtrair \n3 - Multiplicar \n4 - Dividir\n\n"))
+    valorg = int(input("\nInsira o valor para usar na função g(x):\n"))
+    
+    if operadorg == 1:
+      def funcao_g(x):
+        return x + valorg
+      print(f"\n\nFUNÇÃO G(x) = x + {valorg}")
+
+    elif operadorg == 2:
+      def funcao_g(x):
+        return x - valorg
+      print(f"\n\nFUNÇÃO G(x) = x - {valorg}")
+
+    elif operadorg == 3:
+      def funcao_g(x):
+        return x * valorg
+      print(f"\n\nFUNÇÃO G(x) = x ⋅ {valorg}")
+
+    elif operadorg == 4:
+      def funcao_g(x):
+        return x / valorg
+      print(f"\n\nFUNÇÃO G(x) = x ÷ {valorg}")
+    
+    else:
+      print("\nOpção inválida!")
+    
+  
+    valor_de_x = int(input("\n\nInsira o valor de x: "))
     
     print(f"\n\ng ° f({valor_de_x}) = {gf()}")
-    print(f"g ° g({valor_de_x}) = {gg()}")
-    print(f"f ° f({valor_de_x}) = {ff()}")
-    print(f"f ° g({valor_de_x}) = {fg()}")
+    print(f"g ° g ({valor_de_x}) = {gg()}")
+    print(f"f ° f ({valor_de_x}) = {ff()}")
+    print(f"f ° g ({valor_de_x}) = {fg()}")
     
   elif operacoes != 1 and operacoes != 2:
     print("\nOpção inválida!")
